10b3331976a1cd1fa0343e49f6a3b5e1b698d778
shajal <shajal@tryalpine.com>
2022-12-07 23:08:42 +0600

    Changed terraform version

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.2.6" # see https://releases.hashicorp.com/terraform/
+  required_version = ">=0.13.6" # see https://releases.hashicorp.com/terraform/
   required_providers {
     google = {
       source  = "hashicorp/google"
